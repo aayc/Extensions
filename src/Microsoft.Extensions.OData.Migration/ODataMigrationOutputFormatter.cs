1457555bc0b08af1dd1fb7038acbad3eddf3c444
Aaron Chan <aaron.y.chan64@gmail.com>
2019-07-10 09:21:57 -0600

    Getting started on ODataOutputFormatter

---
+++ src/Microsoft.Extensions.OData.Migration/ODataMigrationOutputFormatter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.OData.Formatter;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.OData;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Extensions.OData.Migration
+{
+    public class ODataMigrationOutputFormatter : ODataOutputFormatter
+    {
+        public ODataMigrationOutputFormatter(IEnumerable<ODataPayloadKind> payloadKinds)
+            : base(payloadKinds)
+        {
+        }
+
+        public override bool CanWriteResult(OutputFormatterCanWriteContext context)
+        {
+            bool isODataV3 = context.HttpContext.Request.Headers["odata-service-VERSION-TODO"] == "3.0";
+            return isODataV3 && base.CanWriteResult(context);
+        }
+    }
+}
